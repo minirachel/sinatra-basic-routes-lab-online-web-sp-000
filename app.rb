a181b3d686177823f37dd65c86586dd36262a922
minirachel <rachel.a.levinson@gmail.com>
2021-05-26 12:24:11 -0400

    commit tree 66e4061ebd263fbd2f0ebeb03dbd139c3dda70ff

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        status 200
+        body "My name is "
+        
+    end
+
+    get '/hometown' do
+        status 200
+        body "My hometown is "
+        
+    end
+
+    get '/favorite-song' do
+        status 200
+        "My favorite song is "
+        
+    end
 end
